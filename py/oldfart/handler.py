9897f4e2914d9d6a76e2f6e2408b61c1d55f8856
Matti Hänninen <matti@mjhanninen.com>
2015-01-03 10:50:45 +0200

    First actually working make backed handler
    With hardcoded stuff but nevertheless.

--- py/oldfart/handler.py
+++ py/oldfart/handler.py
@@ -1,6 +1,21 @@
 import http.server
+import os
 
 import oldfart.make
 
+
+maker = oldfart.make.Maker('.')
+
+
 class MakerHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
-    pass
+
+    server_version = 'MakerHTTP/0.1'
+
+    def send_head(self):
+        path = self.translate_path(self.path)
+        target = os.path.relpath(path, os.getcwd())
+        if not os.path.isdir(path):
+            retval, output = maker.make(target)
+            if retval == oldfart.make.FAILURE:
+                self.send_error(500, 'Could not generate resource')
+        return super().send_head()
