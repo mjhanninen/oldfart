b83ae3b2f50cd5c6e35b084871775dc8e9f8b778
Matti Hänninen <matti@mjhanninen.com>
2015-01-03 10:46:10 +0200

    Treat 'no rule' case specially
    Treat the 'no rule' error specially in the case of the immediate target.
    Not all resources are generated via make(1) and, thus, would be missing
    the rule. Further a 404 response to a missing first order rule seems
    more appropriate than a hard 500 build failure.

--- py/oldfart/make.py
+++ py/oldfart/make.py
@@ -6,9 +6,10 @@ import subprocess
 __all__ = ['NOOP', 'SUCCESS', 'FAIL', 'Maker']
 
 
-NOOP = 1
+NOTHING_DONE = 1
 SUCCESS = 2
-FAIL = 3
+NO_RULE = 3
+FAILURE = 4
 
 
 class Maker(object):
@@ -17,25 +18,30 @@ class Maker(object):
         self._project_dir = os.path.abspath(project_dir)
         self._makefile = os.path.abspath(os.path.join(project_dir, makefile))
 
-    def make(self, resource):
-        """Runs `make(1)` on `resource` and returning a tuple `(status, output)`
+    def make(self, target):
+        """Runs `make(1)` on `target` and returning a tuple `(status, output)`
         where `status` is one of:
 
-        - `make.SUCCESS`: the resource was successfully generated
-        - `make.NOOP`: the resource was already up-to-date
-        - `make.FAIL`: `make(1)` exited with a non-zero error code.
+        - `make.SUCCESS`: the target was successfully generated
+        - `make.NOTHING_DONE`: the target was already up-to-date
+        - `make.NO_RULE`: there is no rule to build the requested target
+        - `make.FAILURE`: `make(1)` exited otherwise with a non-zero error code
 
         Returned `output` contains always the mixed output from `stdout` and
         `stderr`.
         """
         try:
             capture = subprocess.check_output(
-                ['make', '--makefile=' + self._makefile, resource],
+                ['make', '--makefile=' + self._makefile, target],
                 cwd=self._project_dir, stderr=subprocess.STDOUT,
                 universal_newlines=True)
-            if re.match(r"make: `[^']*' is up to date.", capture) is not None:
-                return (NOOP, capture)
+            if re.match(r"make: `[^']*' is up to date.", capture):
+                return (NOTHING_DONE, capture)
             else:
                 return (SUCCESS, capture)
         except subprocess.CalledProcessError as e:
-            return (FAIL, e.output)
+            if re.match(r"make: \*\*\* No rule to make target `{:s}'.  Stop."
+                        .format(target), e.output):
+                return (NO_RULE, e.output)
+            else:
+                return (FAILURE, e.output)
