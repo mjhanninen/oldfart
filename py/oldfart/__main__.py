8c57bf35c5de0c710b963382f27442fe485c54ab
Matti Hänninen <matti@mjhanninen.com>
2015-01-03 14:13:15 +0200

    Move hardcoded maker to main module

--- py/oldfart/__main__.py
+++ py/oldfart/__main__.py
@@ -1,10 +1,13 @@
 import socketserver
 
 import oldfart.handler
+import oldfart.make
 
 PORT = 8000
 
-Handler = oldfart.handler.MakerHTTPRequestHandler
+maker = oldfart.make.Maker('.')
+
+Handler = oldfart.handler.make_http_request_handler_class('MakerHTTP', maker)
 
 # NB: Enforce local loopback interface
 httpd = socketserver.TCPServer(("127.0.0.1", PORT), Handler)
--- py/oldfart/handler.py
+++ py/oldfart/handler.py
@@ -4,18 +4,21 @@ import os
 import oldfart.make
 
 
-maker = oldfart.make.Maker('.')
+__all__ = ['make_http_request_handler_class']
 
 
-class MakerHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
+def _send_head(self):
+    path = self.translate_path(self.path)
+    target = os.path.relpath(path, os.getcwd())
+    if not os.path.isdir(path):
+        retval, output = self.maker.make(target)
+        if retval == oldfart.make.FAILURE:
+            self.send_error(500, 'Could not generate resource')
+    return super(self.__class__, self).send_head()
 
-    server_version = 'MakerHTTP/0.1'
-
-    def send_head(self):
-        path = self.translate_path(self.path)
-        target = os.path.relpath(path, os.getcwd())
-        if not os.path.isdir(path):
-            retval, output = maker.make(target)
-            if retval == oldfart.make.FAILURE:
-                self.send_error(500, 'Could not generate resource')
-        return super().send_head()
+def make_http_request_handler_class(name, maker):
+    cls = type(name, (http.server.SimpleHTTPRequestHandler,), {
+        'maker': maker,
+        'send_head': _send_head
+    })
+    return cls
