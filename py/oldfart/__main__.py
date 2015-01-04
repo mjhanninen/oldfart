3b201ec0109595312002bf403083419ac0b51c12
Matti Hänninen <matti@mjhanninen.com>
2015-01-04 13:26:37 +0200

    Display basic info during start up

--- py/oldfart/__main__.py
+++ py/oldfart/__main__.py
@@ -9,6 +9,9 @@ project_dir = os.path.abspath('example')
 server_dir = os.path.join(project_dir, 'build', 'dist')
 port = 8000
 
+# NB: Enforce local loopback interface
+host = '127.0.0.1'
+
 maker = oldfart.make.Maker(project_dir, makefile)
 
 Handler = oldfart.handler.make_http_request_handler_class('MakerHTTP', maker)
@@ -17,7 +20,8 @@ if not os.path.exists(server_dir):
     os.makedirs(server_dir)
 os.chdir(server_dir)
 
-# NB: Enforce local loopback interface
-httpd = socketserver.TCPServer(("127.0.0.1", port), Handler)
+httpd = socketserver.TCPServer((host, port), Handler)
+
+print('Serving "{:s}" through {:s}:{:d}'.format(server_dir, host, port))
 
 httpd.serve_forever()
