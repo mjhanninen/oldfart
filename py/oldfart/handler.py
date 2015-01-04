ede601daaeb75a1cba1a57b9b22486ab828707d6
Matti Hänninen <matti@mjhanninen.com>
2015-01-04 08:54:27 +0200

    Log build process output
    When there is an error or the rule for building the resource was
    missing.

--- py/oldfart/handler.py
+++ py/oldfart/handler.py
@@ -14,8 +14,11 @@ def _send_head(self):
     if not os.path.isdir(path):
         retval, output = self.maker.make(target)
         if retval == oldfart.make.FAILURE:
+            self.log_error('Building resource failed:\n%s', output)
             self.send_error(500, 'Could not generate resource')
             return None
+        elif retval == oldfart.make.NO_RULE:
+            self.log_message('No rule for building the resource')
     return super(self.__class__, self).send_head()
 
 def make_http_request_handler_class(name, maker):
